66365fde95a7dfe37d48a0a99c0865d16e965f0e
Sylvain Le Courtois <sylvain.lecourtois@gmail.com>
2021-04-02 16:11:59 +0200

    Create 2020-01-01-why-I-blog-about-shadertoy.cmd

---
+++ _posts/2020-01-01-why-I-blog-about-shadertoy.cmd
@@ -0,0 +1,18 @@
+---
+layout: default
+title: Why I felt the need to blog about Shadertoy
+tags: shadertoy
+---
+The idea of this notebook is to references some usefull information I found in the shaders of Shadertoy.
+Hopefully this also may help someone to make cool things on this web site.
+
+There is about 50 000 shaders published, and the code is accessible but seems like magic stuffs if you don't know the theory behind. My idea is to reference the ones that I studied in details or just noticed for later.
+I think that you don't need to be an senior programmer in order to start with shadertoy. The proof is that I'am not working as a developer currenlty. It's far simplier to learn GLSL than to learn to play Piano for example (I tried).
+
+This may be a great initiation for young programmers, the same way I did my firt programs on my ATARI 520 ST. 
+
+But in order to really get fun, you should interested in maths, problems, aesthetic in curves, nature and science in general. This is at least my opinion.
+
+You need also to learn some mathematical basis teached in engeneer's preparation schools.
+
+You should be also sensible to short programming and **performance optimization**. It's more programming for **physicians** than for business software engeneers. For this, **you should be informed about the underlying hardware architecture**. (the GPU)
